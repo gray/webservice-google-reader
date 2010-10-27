455ddaf2b88f564dce20b2b7e31f43cc66f400ef
gray <gray@cpan.org>
2010-10-26 17:19:43 -0700

    block/redo is more legible than goto LABEl or do/while

--- ex/mark-dead-craigslist-posts.pl
+++ ex/mark-dead-craigslist-posts.pl
@@ -21,25 +21,26 @@ $|= 1;
 
 my ($total, $expired) = (0, 0);
 
-FEED:
-for my $entry ($feed->entries) {
-    printf "%s total, %s dead\r", ++$total, $expired;
-    next unless $entry->link and $entry->link->href;
-    my $res = $reader->ua->get(
-        $entry->link->href,
-        user_agent => 'Google Reader dead post marker/0.01'
-    );
-    next unless $res->is_success;
-    next unless $res->decoded_content =~ m[
-        <h2>this\ posting\ has\ (?:
-            expired\. |
-            been\ deleted\ by\ its\ author\. |
-            been\ <a[^>]+>flagged</a>\ for\ removal
-        )</h2>
-    ]ix;
-    $reader->mark_read_entry($entry);
-    printf "%s total, %s dead\r", $total, ++$expired;
-}
-continue { sleep 0.25 }
+{
+    for my $entry ($feed->entries) {
+        printf "%s total, %s dead\r", ++$total, $expired;
+        next unless $entry->link and $entry->link->href;
+        my $res = $reader->ua->get(
+            $entry->link->href,
+            user_agent => 'Google Reader dead post marker/0.01'
+        );
+        next unless $res->is_success;
+        next unless $res->decoded_content =~ m[
+            <h2>this\ posting\ has\ (?:
+                expired\. |
+                been\ deleted\ by\ its\ author\. |
+                been\ <a[^>]+>flagged</a>\ for\ removal
+            )</h2>
+        ]ix;
+        $reader->mark_read_entry($entry);
+        printf "%s total, %s dead\r", $total, ++$expired;
+    }
+    continue { sleep 0.25 }
 
-goto FEED if $reader->more($feed);
+    redo if $reader->more($feed);
+}
