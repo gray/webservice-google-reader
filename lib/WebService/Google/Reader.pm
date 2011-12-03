7c64f28f0652f5e68ee054b9ecebbe5ed83a61ef
gray <gray@cpan.org>
2011-12-02 22:55:42 -0800

    bump version for new release

--- lib/WebService/Google/Reader.pm
+++ lib/WebService/Google/Reader.pm
@@ -15,7 +15,7 @@ use WebService::Google::Reader::Constants;
 use WebService::Google::Reader::Feed;
 use WebService::Google::Reader::ListElement;
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 $VERSION = eval $VERSION;
 
 __PACKAGE__->mk_accessors(qw(
